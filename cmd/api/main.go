04beef191b53a7fb8e83d0d4aba197688e52f43b
Krzysztof Piasecki <krzysztof.piasecki@capgemini.com>
2026-07-14 19:36:36 +0200

    refactor(config): use configuration package for application settings

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,33 @@
-
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/kpiasecki/wms/internal/config"
+)
 
 func main() {
-    fmt.Println("WMS started")
+	conn, err := pgx.Connect(
+		context.Background(),
+		config.DatabaseURL,
+	)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(context.Background())
+
+	var dbName string
+
+	err = conn.QueryRow(
+		context.Background(),
+		"SELECT current_database()",
+	).Scan(&dbName)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to database:", dbName)
 }
---
+++ internal/config/database.go
@@ -0,0 +1,3 @@
+package config
+
+const DatabaseURL = "host=localhost port=5432 user=wms_user password=wms_password dbname=wms sslmode=disable"
